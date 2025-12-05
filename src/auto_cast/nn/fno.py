Fix FNO config, add normalization config

src/auto_cast/nn/fno.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 from typing import Any, Protocol, runtime_checkable
 
 import torch
@@ -26,7 +27,7 @@ class FNOProcessor(Processor):
         Number of input channels.
     out_channels: int
         Number of output channels.
-    n_modes: tuple[int, ...]
+    n_modes: Sequence[int]
         Number of Fourier modes to keep in each spatial dimension.
     hidden_channels: int, optional
         Width of the FNO (number of channels in hidden layers). Default is 64.
@@ -53,7 +54,7 @@ class FNOProcessor(Processor):
         self,
         in_channels: int,
         out_channels: int,
-        n_modes: tuple[int, ...],
+        n_modes: Sequence[int],
         hidden_channels: int = 64,
         n_layers: int = 4,
         loss_func: nn.Module | None = None,
@@ -62,8 +63,10 @@ class FNOProcessor(Processor):
     ):
         super().__init__()
 
+        n_modes_tuple = tuple(n_modes)
+
         self.model = FNO(
-            n_modes=n_modes,
+            n_modes=n_modes_tuple,
             in_channels=in_channels,
             out_channels=out_channels,
             hidden_channels=hidden_channels,