2b5178ba2fe69c7548771bdab9fbcdb7aed2d11e
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-12-08 18:17:50 +0000

    Update FNO
    Add stride and max_rollout_steps to FNO

--- src/auto_cast/nn/fno.py
+++ src/auto_cast/nn/fno.py
@@ -58,6 +58,8 @@ class FNOProcessor(Processor):
         n_layers: int = 4,
         loss_func: nn.Module | None = None,
         learning_rate: float = 1e-3,
+        stride: int = 1,
+        max_rollout_steps: int = 10,
         **fno_kwargs: Any,
     ):
         super().__init__()
@@ -72,6 +74,8 @@ class FNOProcessor(Processor):
         )
         self.loss_func = loss_func or nn.MSELoss()
         self.learning_rate = learning_rate
+        self.stride = stride
+        self.max_rollout_steps = max_rollout_steps
         self._reset_positional_embedding_cache()
 
     def forward(self, x: Tensor) -> Tensor:
