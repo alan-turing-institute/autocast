2a258fd9851e9c137b0a058cb20deea401a1cac6
Paolo Conti <pconti@turing.ac.uk>
2025-12-09 09:43:04 +0000

    updated pytest for metrics

--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -1,27 +1,16 @@
 import pytest
 import torch
 
-from auto_cast.metrics.spatiotemporal import (
-    MAE,
-    MSE,
-    NMAE,
-    NMSE,
-    NRMSE,
-    RMSE,
-    VMSE,
-    VRMSE,
-)
+from auto_cast.metrics import ALL_METRICS
 from auto_cast.types import TensorBTSPlusC
 
 
-@pytest.mark.parametrize(
-    "metric",
-    (MSE(), MAE(), NMAE(), NMSE(), NRMSE(), RMSE(), VRMSE(), VMSE()),
-)
+@pytest.mark.parametrize("metric", ALL_METRICS)
+
 def test_spatiotemporal_metrics(metric):
     y_pred: TensorBTSPlusC = torch.ones((2, 3, 4, 5))
     y_true: TensorBTSPlusC = torch.ones((2, 3, 4, 5))
     n_spatial_dims = 1
 
-    error = metric(y_pred, y_true, n_spatial_dims)
+    error =  metric()(y_pred, y_true, n_spatial_dims)
     assert torch.allclose(error.nansum(), torch.tensor(0.0))
