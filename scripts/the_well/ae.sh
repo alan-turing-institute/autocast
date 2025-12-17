20b54438782819b2b2c6378ff01f1e97fd218383
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-12-17 07:35:31 +0000

    Reduce time

--- scripts/the_well/ae.sh
+++ scripts/the_well/ae.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=vjgo8416-ai-phy-sys
 #SBATCH --qos turing
-#SBATCH --time 24:00:00
+#SBATCH --time 12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=36
