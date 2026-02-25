3291b962c701b6e81e1c3079e0a178765683dd9c
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-02-25 09:44:11 +0000

    Add dataset name tokens for Gray-Scott

--- src/autocast/scripts/workflow/constants.py
+++ src/autocast/scripts/workflow/constants.py
@@ -21,4 +21,8 @@ DATASET_NAME_TOKENS: dict[str, str] = {
     "advection_diffusion_multichannel": "adm32",
     "advection_diffusion_singlechannel": "ad32",
     "reaction_diffusion": "rd32",
+    "gray_scott_spirals": "gss64",
+    "gray_scott_gliders": "gsg64",
+    "gray_scott_range_spirals": "gsrs64",
+    "gray_scott_range_gliders": "gsrg64",
 }
