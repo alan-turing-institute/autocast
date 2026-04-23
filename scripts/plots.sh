becad04d8feaaa6e3179151629ec078e405b0771
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-04-23 14:53:39 +0100

    Add no FM ambient version

--- scripts/plots.sh
+++ scripts/plots.sh
@@ -77,6 +77,28 @@ autocast-plots --results-dir outputs/2026-04-20_collated \
 	--panel-figure-no-training \
 	--output-dir outputs/2026-04-20_collated/$PLOTS_PATH/main_comparison_m16_complete
 
+# Main comparison (with m=16 runs 100^ complete): CRPS ambient variants, CRPS processor-on-latents, FM ambient (EPD)
+autocast-plots --results-dir outputs/2026-04-20_collated \
+	--run crps_ad64_vit_azula_large_ac1bb06_ef6368d "CRPS (ambient, m=16)" 0 \
+	--run crps_cns64_vit_azula_large_0db40e1_dcd79e4 "CRPS (ambient, m=16)" 0 \
+	--run crps_gpe64_vit_azula_large_ac1bb06_638585e "CRPS (ambient, m=16)" 0 \
+	--run crps_gs64_vit_azula_large_ac1bb06_639963f "CRPS (ambient, m=16) " 0 \
+	--run diff_ad64_flow_matching_vit_09490da_dae1382 "FM (latent)" 2 \
+	--run diff_cns64_flow_matching_vit_09490da_636fcc3 "FM (latent)" 2 \
+	--run diff_gpe64_flow_matching_vit_09490da_47bf39a "FM (latent)" 2 \
+	--run diff_gs64_flow_matching_vit_09490da_7e9e331 "FM (latent)" 2 \
+	--dataset-order AD CNS GS GPE\
+	--error-ylim 1e-5 1 \
+	--lead-time-error-metrics vrmse rmse \
+	--lead-time-coverage-metrics coverage_0.9 coverage_0.5 coverage_0.1 \
+	--lead-time-coverage-delta \
+	--combined-lead-time \
+	--training-metrics val_loss train_loss \
+	--training-yscale log \
+	--panel-figure \
+	--panel-figure-no-training \
+	--output-dir outputs/2026-04-20_collated/$PLOTS_PATH/main_comparison_m16_complete_no_fm_amb
+
 # Main comparison (with m=16 runs at 75% checkpoint): CRPS ambient variants, CRPS processor-on-latents, FM ambient (EPD)
 autocast-plots --results-dir outputs/2026-04-20_collated \
 	--run crps_ad64_vit_azula_large_ac1bb06_ef6368d "CRPS (ambient, m=16)" 0 eval=eval_0p75 \
