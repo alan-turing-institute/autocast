3f113f2c1739d359cc51f52a9dceaf673c39bc30
Sam Greenbury <sgreenbury@turing.ac.uk>
2026-05-15 15:22:58 +0000

    Add masked-window FM processor
    Add a flow-matching processor variant that masks temporal windows
    so RB experiments can train with the masked-window objective.

--- src/autocast/processors/__init__.py
+++ src/autocast/processors/__init__.py
@@ -1,11 +1,15 @@
 from autocast.processors.azula_vit import AzulaViTProcessor
 from autocast.processors.base import Processor
 from autocast.processors.flow_matching import FlowMatchingProcessor
+from autocast.processors.flow_matching_masked_window import (
+    FlowMatchingMaskedWindowProcessor,
+)
 from autocast.processors.swin_vit import SwinViTProcessor
 from autocast.processors.unet import UNetProcessor
 
 __all__ = [
     "AzulaViTProcessor",
+    "FlowMatchingMaskedWindowProcessor",
     "FlowMatchingProcessor",
     "Processor",
     "SwinViTProcessor",
